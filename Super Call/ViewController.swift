96b2b19edeb566dc493f5c2dd637710aaf5d2f63
Malek <MA123@Maleks-Mac.local>
2014-12-20 04:55:51 +0200

    Initial Commit

---
+++ Super Call/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Call
+//
+//  Created by Malek on 12/20/14.
+//  Copyright (c) 2014 Malek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
